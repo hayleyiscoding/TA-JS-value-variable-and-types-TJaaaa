fcfc8151e8aa90f8bb1e16e4b04464135324dacd
Hayley Wood <onlinecowork@gmail.com>
2020-12-29 01:21:49 +1000

    Javascript loops

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -6,22 +6,61 @@
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
 
+let user = prompt("What is your name?");
+alert(user);
+let userName = user;
+console.log(user);
+console.log(userName);
+
+let age = 22; isLearning = true;
+user = "John";
+console.log (user);
+console.log (userName);
+
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
+
+let numA = Number(prompt("Enter a number"));
+
+let numB = Number(prompt("Enter another number"));
+
+let sum = numA + numB;
+
+alert(sum);
 
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+for (let j = 0; j <= 10; j++) {
+    if (j % 2 === 0) {
+        console.log(j);
+    }
+}
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
 
+for (let i = 9; i >= 1; i--) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
+
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
+
+let i = 5;
+while (i >= 0) {
+    let sum = 5;
+    sum =- 1;
+    console.log(sum);
+    i--;
+}
 
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
 
+
+r
 // Comparisoin
 
 /* 7. Take two value using prompt and store them in variables `num1` and `num2`. Check whether they are equal or not.
@@ -36,3 +75,14 @@ Example:
 "hello", -21; // false
 
 */
+
+let num1 = Number(prompt("Enter a number"));
+let num2 = Number(prompt("Enter another number"));
+
+if (num1 === num2) {
+    alert("true");
+} else if (num1 !== num2) {
+    alert("false");
+} else if (num1 === 'true' || num1 === 'null' || num1 === "undefined" || num2 === 'true' || num2 === 'null' || num2 === "undefined") {
+    alert("Enter a valid value");
+}
